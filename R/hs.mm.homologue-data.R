cb0837167fcd7dc1b482d0fc7170b56ab00f8692
Nikolas Barkas <nikolas.barkas@outlook.com>
2018-03-22 15:09:26 -0400

    add doc for data

---
+++ R/hs.mm.homologue-data.R
@@ -0,0 +1,11 @@
+#' Mouse to human gene mapping from ensembl
+#'
+#' Data generated with function hommap.mm.2.hs.symbols
+#'
+#' @docType data(hs.mm.homologue)
+#'
+#' @format An object of class \code{'tibble'}
+#'
+#' @keywords datasets
+#'
+"hs.mm.homologue"
